833f78a9778cce32945a073fc42f5ecc86954999
crapola <crapola@users.noreply.github.com>
2019-08-11 17:32:03 +0200

    Works

--- __main__.py
+++ __main__.py
@@ -1,30 +1,37 @@
 from PIL import Image
+import config
 
-def dither(x:int, y:int, i:float, v:float, off:int) -> int:
-	""" off=base """
+def dither(x:int, y:int, i:float, v:float, base:int) -> int:
+	""" v=number of values from base"""
 	k=((1,3),(4,2))
-	return off+(i*v)+ ( 1 if ((v*(i%(1./v))) +k[x%2][y%2]/4.)>=1 else 0 ) 
+	return base+(i*v)+ ( 1 if ((v*(i%(1./v))) +k[x%2][y%2]/4.)>=1 else 0 ) 
 
 def main():
 	image=Image.open("test.png")
-	#image.show()
+	width=image.width
+
 	pixels=tuple(image.getdata())
-	#print(pixels)
+
+
+	# Create list of values from red (since it's a greyscale image).
 	intensities=[x[0] for x in pixels]
-	print(intensities)
+	#print(intensities)
 	
+	result_colors=[]
+
+	for i,x in enumerate(intensities):
+		g=x/255.0
+		red=0#dither(i%width,int(i/width),g,config.size[0],config.base[0])
+		green=dither(i%width,int(i/width),g,config.size[1],config.base[1])
+		blue=dither(i%width,int(i/width),g,config.size[2],config.base[2])
+		color=(int(red),int(green),int(blue))
+		result_colors.append(color)
+
+	#print(result_colors)
+	# Destination image.
 	result=Image.new('RGB',(256,256))
-	result.putdata(intensities)
+	result.putdata(result_colors)
 	result.show()
-
-	w=h=4
-
-	for x in range(w):
-		for y in range(h):
-			for i in [x/10.0 for x in range(10)]:
-				#print(i)
-				print(dither(0,0,i,20,32),end=' ')
-	pass
 
 if __name__=="__main__":
 	main()

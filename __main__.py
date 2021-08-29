5beb47e01cff006e03907b27d67f0ba6fe59081e
Crapola <crapola@users.noreply.github.com>
2021-08-29 17:41:46 +0200

    Numpy

--- __main__.py
+++ __main__.py
@@ -1,45 +1,10 @@
-import concurrent.futures
-import time
 from PIL import Image
+import numpy as np
 import config
-
-def dither(x,y,i,v,base):
-	"""
-	Dither a color component.
-	Parameters
-	----------
-	x,y : int
-		Pixel position.
-	i : float
-		Pixel value in range [0,1].
-	v
-		Number of values from base.
-	base : int
-		Base value for this component.
-	Returns
-	-------
-	int
-		Component value in range [0,255].
-	"""
-	k=((1,3),(4,2))
-	if v==0:
-		return base+(i*v)
-	return base+(i*v)+ ( 1 if ((v*(i%(1./v))) +k[x%2][y%2]/4.)>=1 else 0 ) 
 
 def print_value_range(image):
 	values=tuple(image.getdata())
 	print(f"Value range: {(min(values),max(values))}.")
-
-def work(values_chunk,width):
-	result_colors=[None]*len(values_chunk)
-	for i,x in enumerate(values_chunk):
-		v=x/255.0
-		red=dither(i%width,int(i/width),v,config.size[0],config.base[0])
-		green=dither(i%width,int(i/width),v,config.size[1],config.base[1])
-		blue=dither(i%width,int(i/width),v,config.size[2],config.base[2])
-		color=(int(red),int(green),int(blue))
-		result_colors[i]=color
-	return result_colors
 
 def main():
 	image=Image.open("source.png").convert("L")
@@ -49,17 +14,19 @@ def main():
 	values=tuple(image.getdata())
 	width=image.width
 	height=image.height
-
-	time_spent=time.time()
-	with concurrent.futures.ProcessPoolExecutor(max_workers=8) as executor:
-		n=len(values)//8
-		chunks=[values[i:i+n] for i in range(0,len(values),n)]
-		future=[executor.submit(work,c,width) for c in chunks]
-		result_colors=[f.result() for f in future]
-
-	time_spent=time.time()-time_spent
-	result_colors=sum(result_colors,[])
-	print(f"Time elapsed:{time_spent} seconds.")
+	v=np.array(values)/255.0
+	i=np.arange(len(v))
+	# Dithering.
+	k=np.array((0,2,3,1))
+	thresholds=np.take(k,i%2+2*((i//width)%2))/4.0-0.5
+	# Channels.
+	rs=(config.base[0]+config.size[0]*v+thresholds).astype(int)
+	gs=(config.base[1]+config.size[1]*v+thresholds).astype(int)
+	bs=(config.base[2]+config.size[2]*v+thresholds).astype(int)
+	reds=rs.tolist()
+	greens=gs.tolist()
+	blues=bs.tolist()
+	result_colors=tuple(zip(reds,greens,blues))
 	# Save destination image.
 	result=Image.new('RGB',image.size)
 	result.putdata(result_colors)
